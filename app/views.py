a899e9be307aaabe6219f85d128db9a9957b75fe
Saul Lugo <saullugolugo@gmail.com>
2015-10-01 17:54:52 +0200

    Part III

--- app/views.py
+++ app/views.py
@@ -1,6 +1,30 @@
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'nickname': 'Saul'} #fake user
+	posts = [ #fake array of posts
+		{
+			'author': {'nickname':'John'},
+			'body': 'Beautiful day in Madrid'
+		},
+		{
+			'author': {'nickname':'Carlos'},
+			'body': 'I like the food here'
+		}
+	]
+	return render_template('index.html',
+							title='Home',
+							user=user,
+							posts=posts)
+							
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html',
+							title='Sign In',
+							form=form)
+							
---
+++ config.py
@@ -0,0 +1,6 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+	{'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+	{'name' : 'MyOpenID', 'url': 'http://openid.net/'}]
